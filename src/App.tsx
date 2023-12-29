3c5ba451d7199a22f254a4fe6a13054dfb54392d
Span <spandetectea@gmail.com>
2023-12-30 00:24:44 +0300

    Страница боевки. Начало

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CombatPage from './components/CombatPage/CombatPage';
 
 function App() {
   return (
     <div className="App">
+      <CombatPage/>
     </div>
   );
 }
